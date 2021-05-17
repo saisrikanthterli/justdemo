e5315cafd41edf0ccaa207478129c9f0bc8ccd84
unknown <saisrikanth@gmail.com>
2021-05-17 17:00:35 +0530

    just a space

---
+++ server.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const app = express();
+const port=process.env.PORT||3000;
+
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended:false}));
+app.get('/',(req,res)=>{
+    res.render('idex')
+})
+
+app.post('/',(req,res)=>{
+    console.log(req.body)
+    res.send(req.body)
+})
+
+
+
+app.listen(port)
