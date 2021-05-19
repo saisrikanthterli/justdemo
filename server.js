09099648fed41859a78c20435cde40738787e9c8
unknown <saisrikanth@gmail.com>
2021-05-19 10:36:52 +0530

    changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express=require('express');
 const app = express();
 const port=process.env.PORT||3000;
 
+const users=[{user1: 'john'},{ user1: 'nike'}]
 
 app.set('view engine', 'ejs')
 
@@ -14,8 +15,12 @@ app.get('/',(req,res)=>{
 
 app.post('/',(req,res)=>{
     console.log(req.body)
+    users.push(req.body)
     res.redirect('/searchpage')
+    console.log(users)
+
 })
+
 app.get('/searchpage',(req,res)=>{
     res.render('searchpage')
 })
@@ -23,19 +28,11 @@ app.get('/searchpage',(req,res)=>{
 
 app.post('/searchpage',(req,res)=>{
     
-        const users=[{user1: 'john'},{ user1: 'nike'}]
-        const data=users.find((user)=>{
-    
-            if(user.user1===req.body.query)
-                {
-                    
-                   res.redirect('profile',{name:user.user1})
-                }
-
-            })
-            res.send(data.user1)
-            //  res.send('UnAutherized User....')
+       
+        console.log(req.body)
+            
         })
+        
 app.get('/profile',(req,res)=>{
     res.render('profile')
 })
